6d25f5f25f996d98452814e7425bdc895a496efa
AipuAmeh <aipuameh.16@gmail.com>
2023-04-06 17:56:27 -0400

    updated README

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-// var generateBtn = document.querySelector("password");
+
 var textArea = document.getElementById("password");
 var btn = document.getElementById("generate");
 // Write password to the #password input
@@ -16,9 +16,9 @@ var passwordLength = 0;
 
 // Prompt of criteria questions
   function criteria() {
-    //Ask user how long password needs to be
-    //Check if password is less than 8, return if not
-    //Check if password is greater than 128, return if not
+    // Asks user how long password needs to be
+    // Checks if password is less than 8, return if not
+    // Checks if password is greater than 128, return if not
     var choice5 = window.prompt("How long do you want your password to be? \n Password must be at least 8 characters and no more than 128 characters");
     if (choice5 < 8 || choice5 > 128) {
       return;
@@ -29,15 +29,13 @@ var passwordLength = 0;
     var choice2 = window.confirm("Do you want upper case letters?");
     var choice3 = window.confirm("Do you want numeric characters?");
     var choice4 = window.confirm("Do you want special characters?");
-    // document.getElementById("generate").onclick = function() {criteria};
-
+   
   
 
-// Cancels that option 
+// If user does not click any options return 
     if (!choice1 && !choice2 && !choice3 && !choice4) {
       return;
     }
-// If any of 5 choices are confirmed, call the function generate password
 
 
     if (choice1) {
@@ -59,71 +57,45 @@ var passwordLength = 0;
     return passwordOptions;
     //return possible characters
 
-
   }
 
 function generatePassword() {
 
   var passwordOptions = criteria();
     var finalPassword = [];
-  //Criteria is going to return array of all possible characters
+// Criteria function is going to return array of all possible characters
   
-    // var choice5 = promptLength();
+  
     if (!passwordOptions) {
       return "Please try again";
     }
  
-    console.log(passwordOptions);
-    // var code = upperCase.concat(lowerCase, num, characters);
-    //Using users choice of length as stopping point, loop through userCharChoices 
-    //and randomly choose an index
-    
-
+    console.log(passwordOptions); // Prints password array to console
+   
+// Loop that uses users choice of length as a stopping point and loops through choices to 
+// randomly choose an index
    for (i = 0; i < passwordOptions.passwordLength; i++) {
-    // if (passwordOptions.passwordLength >= 8 || passwordOptions.passwordLength <= 128) {
       var randomIndex = Math.floor(Math.random() * passwordOptions.possibleCharacters.length);
       var randomChar = passwordOptions.possibleCharacters[randomIndex];
       finalPassword.push(randomChar);
-      // finalPassword.join("");
-    // } 
+  
     }
     console.log(finalPassword);
   
     return finalPassword;
 
     }
-  //  while(userCharChoices < userCharChoices.length) {
-    
-  //  }
-      
-   
-  
-  
-  //
+
+// Starter code prints password to text area at the id #password
   function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password.join("");
 
 
-//  document.getElementById("password").addEventListener("click", writePassword);
-  // = function() {writePassword};
 }
 
-// function promptLength() {
-//   var choice5 = window.prompt("How long do you want your password to be? \n Password must be at least 8 characters and no more than 128 characters");
-//   if (choice5 < 8 || choice5 > 128) {
-//     return;
-//   } 
-//   return choice5;
-// }
 
-
-// if (=== computer) {
-//   (writePassword(password) === computer)
-// }
-
-// Add event listener to generate button
 
 btn.addEventListener("click", writePassword);
 
