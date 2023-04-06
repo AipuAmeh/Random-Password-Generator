bb67ffd92328b7a4a523d30faffa3cc6c75e77cf
AipuAmeh <aipuameh.16@gmail.com>
2023-04-05 22:25:34 -0400

    generated password

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,7 @@ var num = [1, 2, 3, 4, 5, 6, 7, 8];
 var characters = ["!", "#", "$", "&", "/", "?", "+", "@"];
 var possibleCharacters = [];
 var newPassword = "";
+var passwordLength = 0;
 
 
 // Prompt of criteria questions
@@ -18,11 +19,19 @@ var newPassword = "";
     //Ask user how long password needs to be
     //Check if password is less than 8, return if not
     //Check if password is greater than 128, return if not
+    var choice5 = window.prompt("How long do you want your password to be? \n Password must be at least 8 characters and no more than 128 characters");
+    if (choice5 < 8 || choice5 > 128) {
+      return;
+    } 
+    passwordLength = choice5;
+
     var choice1 = window.confirm("Do you want lower case letters?");
     var choice2 = window.confirm("Do you want upper case letters?");
     var choice3 = window.confirm("Do you want numeric characters?");
     var choice4 = window.confirm("Do you want special characters?");
     // document.getElementById("generate").onclick = function() {criteria};
+
+  
 
 // Cancels that option 
     if (!choice1 && !choice2 && !choice3 && !choice4) {
@@ -43,56 +52,71 @@ var newPassword = "";
     if (choice4) {
       possibleCharacters = possibleCharacters.concat(characters)
     } 
-    return possibleCharacters;
-  
+    var passwordOptions = {
+      passwordLength: passwordLength, 
+      possibleCharacters: possibleCharacters,
+    };
+    return passwordOptions;
     //return possible characters
+
 
   }
 
 function generatePassword() {
 
+  var passwordOptions = criteria();
     var finalPassword = [];
   //Criteria is going to return array of all possible characters
-    var userCharChoices = criteria();
-    if (!userCharChoices) {
+  
+    // var choice5 = promptLength();
+    if (!passwordOptions) {
       return "Please try again";
     }
-    var passLength = promptLength();
-    if (!passLength) {
-      return "Please try again"
-    }
-    console.log(userCharChoices);
+ 
+    console.log(passwordOptions);
     // var code = upperCase.concat(lowerCase, num, characters);
     //Using users choice of length as stopping point, loop through userCharChoices 
     //and randomly choose an index
-    var randomIndex = Math.floor(Math.random() * userCharChoices.length);
-    var randomChar = userCharChoices[randomIndex];
-    var count =0;
     
-   while(userCharChoices < userCharChoices.length) {
-    finalPassword.push(randomChar);
-    return finalPassword.join("");
-   }
+
+   for (i = 0; i < passwordOptions.passwordLength; i++) {
+    // if (passwordOptions.passwordLength >= 8 || passwordOptions.passwordLength <= 128) {
+      var randomIndex = Math.floor(Math.random() * passwordOptions.possibleCharacters.length);
+      var randomChar = passwordOptions.possibleCharacters[randomIndex];
+      finalPassword.push(randomChar);
+      // finalPassword.join("");
+    // } 
+    }
+    console.log(finalPassword);
+  
+    return finalPassword;
+
+    }
+  //  while(userCharChoices < userCharChoices.length) {
+    
+  //  }
       
    
-  }
+  
   
   //
   function writePassword() {
-  textArea.value = generatePassword();
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password.join("");
 
 
 //  document.getElementById("password").addEventListener("click", writePassword);
   // = function() {writePassword};
 }
 
-function promptLength() {
-  var choice5 = window.prompt("How long do you want your password to be? \n Password must be at least 8 characters and no more than 128 characters");
-  if (choice5 < 8 || choice5 > 128) {
-    return;
-  } 
-  return choice5;
-}
+// function promptLength() {
+//   var choice5 = window.prompt("How long do you want your password to be? \n Password must be at least 8 characters and no more than 128 characters");
+//   if (choice5 < 8 || choice5 > 128) {
+//     return;
+//   } 
+//   return choice5;
+// }
 
 
 // if (=== computer) {
